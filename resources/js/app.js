8cc65141dc5e5259ad01c25151a81d9a8da49bc9
José Carlos <jcolscript@gmail.com>
2020-08-23 22:52:22 -0300

    finish design

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,34 +1,33 @@
 import "../scss/app.scss";
 
 var renderContactList = async function () {
+  var templateList = document.getElementById("contact_list_template").content;
+  var contactList = document.getElementById("contact_list");
+  var list = (await getContactList()) || [];
 
-	var templateList 	= document.getElementById('contact_list_template').content;
-	var contactList 	= document.getElementById('contact_list');
-	var list 			= await getContactList() || [];
+  console.log(list);
+  list.results.forEach(function (item) {
+    var template = templateList.cloneNode(true);
 
-	console.log(list);
-	list.results.forEach(function (item) {
+    template
+      .querySelector("a")
+      .setAttribute("title", item.name.first + " " + item.name.last);
+    template.querySelector("img").setAttribute("src", item.picture.medium);
 
-		var template = templateList.cloneNode(true);
-
-		template.querySelector('a').setAttribute('title', item.name.first + ' ' + item.name.last);
-		template.querySelector('img').setAttribute('src', item.picture.medium);
-
-		contactList.appendChild(template);
-	});
-}
+    contactList.appendChild(template);
+  });
+};
 
 var getContactList = function () {
-
-	return fetch('https://randomuser.me/api/?results=13').then(function (response) {
-
-		return response.json();
-	});
-}
+  return fetch("https://randomuser.me/api/?results=9").then(function (
+    response
+  ) {
+    return response.json();
+  });
+};
 
 var init = function () {
-
-	renderContactList();
-}
+  renderContactList();
+};
 
 init();
